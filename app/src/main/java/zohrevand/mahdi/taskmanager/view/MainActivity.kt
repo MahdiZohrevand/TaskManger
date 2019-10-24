1acf7cc70fd4441fed799121d29817ce97d21a38
Mahdi <zohrevand.mahdi@gmial.com>
2019-10-24 15:01:31 +0330

    navigation with back button

--- app/src/main/java/zohrevand/mahdi/taskmanager/view/MainActivity.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/view/MainActivity.kt
@@ -1,59 +1,40 @@
 package zohrevand.mahdi.taskmanager.view
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.navigation.findNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.ui.*
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import zohrevand.mahdi.taskmanager.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
 
 
+        drawerLayout = findViewById(R.id.drawer_layout)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
 
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_new_task, R.id.nav_timer,
-                R.id.nav_statistic, R.id.nav_tasks, R.id.nav_send, R.id.nav_share
-            ), drawerLayout
-        )
-
-
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        supportActionBar?.setDisplayShowHomeEnabled(false)
-        NavigationUI.setupActionBarWithNavController(this,navController)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
 
     }
-
-    /* override fun onCreateOptionsMenu(menu: Menu): Boolean {
-         // Inflate the menu; this adds items to the action bar if it is present.
-         menuInflater.inflate(R.menu.drawer, menu)
-         return true
-     }*/
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+
     }
+
 }
