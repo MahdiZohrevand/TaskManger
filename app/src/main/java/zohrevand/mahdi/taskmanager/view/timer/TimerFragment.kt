2984ebdaadccee9f509f37bba9922977feac328d
Mahdi <zohrevand.mahdi@gmial.com>
2019-10-27 19:01:43 +0330

    change name to timer

--- app/src/main/java/zohrevand/mahdi/taskmanager/view/timer/TimerFragment.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/view/timer/TimerFragment.kt
@@ -8,24 +8,30 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import zohrevand.mahdi.taskmanager.R
 
-class SlideshowFragment : Fragment() {
+class TimerFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var timerViewModel: TimerViewModel
+    private val args : TimerFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-            ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+        timerViewModel =
+            ViewModelProviders.of(this).get(TimerViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(this, Observer {
-            textView.text = it
+        timerViewModel.text.observe(this, Observer {
+          //  textView.text = it
         })
+        val item = args.task
+        item.let {
+            textView.text = it.toString()
+        }
         return root
     }
 }
