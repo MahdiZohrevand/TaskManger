5330189e4269918fd6f52ad407a8c89c2eafa6db
Mahdi <zohrevand.mahdi@gmial.com>
2019-11-21 15:23:16 +0330

    move age calculate logic to helper class

--- app/src/main/java/zohrevand/mahdi/taskmanager/view/age/AgeViewModel.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/view/age/AgeViewModel.kt
@@ -4,43 +4,61 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import zohrevand.mahdi.taskmanager.dataAccess.preferences.UserAgePreferenceImp
 import zohrevand.mahdi.taskmanager.utils.PersianCalendar
+import zohrevand.mahdi.taskmanager.utils.ageInMillisecond
+import zohrevand.mahdi.taskmanager.utils.calculateAge
 import java.util.*
+
+//TODO preferenceImp must be replace with interface not implementation
 
 class AgeViewModel(val preferenceImp: UserAgePreferenceImp) : ViewModel() {
 
-    var year: Int = -1
-    var month: Int = -1
-    var day: Int = -1
+    private var year: Int = -1
+    private var month: Int = -1
+    private var day: Int = -1
+
+
+    val yearLiveData = MutableLiveData<Int>()
+    val monthLiveData = MutableLiveData<Int>()
+    val dayLiveData = MutableLiveData<Int>()
+
+    var spinnerYearPosition: Int = 1
+    var spinnerMonthPosition: Int = 1
+    var spinnerDayPosition: Int = 1
 
     val userAge = MutableLiveData<String>()
 
 
-
     init {
         val birthDateInMilli = preferenceImp.getUserBirthdateInMillisconds()
+        //user have been used age calculator and show last used calculated value
         if (birthDateInMilli > 1) {
-            val birthDate = Calendar.getInstance()
-            birthDate.timeInMillis = birthDateInMilli
-            val rightNow = Calendar.getInstance()
-            val diff = PersianCalendar.getPeriodOfTwoTime(birthDate, rightNow)
-            userAge.value = "${diff.years} سال ${diff.months} ماه ${diff.days} روز "
-
+            calculateAge(birthDateInMilli) { year, month, day, age ->
+                spinnerYearPosition = year
+                spinnerMonthPosition = month
+                spinnerDayPosition = day
+                userAge.value = age
+            }
         }
+
+
+        yearLiveData.observeForever {
+            year = it
+        }
+
+        monthLiveData.observeForever {
+            month = it
+        }
+
+        dayLiveData.observeForever {
+            day = it
+        }
+
     }
 
 
-    fun calculateAge() {
-        val birthDate = PersianCalendar.getGregorianCalendar(year, month, day)
-        preferenceImp.setUserBirthdateInMillisconds(birthDate.timeInMillis)
-        val rightNow = Calendar.getInstance()
-        val diff = PersianCalendar.getPeriodOfTwoTime(birthDate, rightNow)
-
-        userAge.value = "${diff.years} سال ${diff.months} ماه ${diff.days} روز "
-    }
-
-
-    fun setSpinnerDate(spinner: (year: Int, month: Int, day: Int) -> Unit) {
-        spinner(1, 2, 3)
+    fun onCalculateClick() {
+        preferenceImp.setUserBirthdateInMillisconds(ageInMillisecond(year, month, day))
+        userAge.value = calculateAge(year, month, day)
     }
 
 }
