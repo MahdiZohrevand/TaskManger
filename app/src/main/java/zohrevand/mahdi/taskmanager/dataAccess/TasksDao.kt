07def8e516e9ee855b7496eac10061f8460f1a26
Mahdi <zohrevand.mahdi@gmial.com>
2019-12-15 15:33:50 +0330

    omit suspend method in task dao

--- app/src/main/java/zohrevand/mahdi/taskmanager/dataAccess/TasksDao.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/dataAccess/TasksDao.kt
@@ -8,16 +8,16 @@ import androidx.room.*
 interface TasksDao {
 
     @Insert
-    suspend fun insert(taskModel: TaskModel)
+    fun insert(taskModel: TaskModel)
 
     @Update
-    suspend fun update(taskModel: TaskModel)
+    fun update(taskModel: TaskModel)
 
     @Delete
-    suspend fun delete(taskModel: TaskModel)
+    fun delete(taskModel: TaskModel)
 
     @Query("SELECT * from task_table WHERE taskId = :key")
-    suspend fun get(key: Long): TaskModel?
+    fun get(key: Long): TaskModel?
 
     @Query("SELECT * from task_table ORDER BY create_date_milli DESC")
     fun getAllTask(): LiveData<List<TaskModel>>
