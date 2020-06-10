09b63bf076213e80cb556c7b915052262ab5e7dc
MahdiZohrevand <nahavandi.arash1313@gmail.com>
2020-06-10 13:22:11 +0430

    make day view adapter
    make setTask fun for adding task to day view

--- app/src/main/java/zohrevand/mahdi/taskmanager/utils/DayViewBindAdapter.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/utils/DayViewBindAdapter.kt
@@ -1,2 +1,11 @@
 package zohrevand.mahdi.taskmanager.utils
 
+import androidx.databinding.BindingAdapter
+import zohrevand.mahdi.customviewtest.model.Task
+import zohrevand.mahdi.dayview.view.DayView
+
+
+@BindingAdapter("setTasks")
+fun setDayViewTasks(dayView: DayView , tasks: List<Task>){
+    dayView.addCalendarTask(tasks)
+}
