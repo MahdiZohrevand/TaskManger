9c8c7245d46583bd1094e83cea3ddd02ca305ba9
mahdi zohrevand <zohrevand.mahdi@gmail.com>
2020-10-05 11:03:19 +0330

    list converter

---
+++ app/src/main/java/zohrevand/mahdi/taskmanager/dataAccess/DBListConverter.kt
@@ -0,0 +1,31 @@
+package zohrevand.mahdi.taskmanager.dataAccess
+
+import android.graphics.Color
+import zohrevand.mahdi.customviewtest.model.Task
+import java.util.*
+
+public fun convertDbTaskListToDayViewTaskList(list: List<TaskModel>): MutableList<Task> {
+    val taskList = mutableListOf<Task>()
+    val calendar = Calendar.getInstance()
+    list.forEach {
+
+        taskList.add(
+            Task(
+                it.TaskId,
+                it.title,
+                it.description,
+                calendar.apply { timeInMillis = it.startDate.time }.get(Calendar.HOUR_OF_DAY)
+                    .toFloat(),
+                calendar.get(Calendar.MINUTE).toFloat(),
+                calendar.apply { timeInMillis = it.finishDate!!.time }.get(Calendar.HOUR_OF_DAY)
+                    .toFloat(),
+                calendar.get(Calendar.MINUTE).toFloat(),
+                Color.parseColor("#e44285F4")
+
+            )
+        )
+    }
+
+    return taskList
+
+}
