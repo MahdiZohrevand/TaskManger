cbb35272dc21beb7ff7d4647c9083302397a5e23
MahdiZohrevand <nahavandi.arash1313@gmail.com>
2020-10-12 19:26:21 +0330

    add go to today

--- app/src/main/java/zohrevand/mahdi/taskmanager/view/taskList/TaskListFragment.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/view/taskList/TaskListFragment.kt
@@ -1,28 +1,22 @@
 package zohrevand.mahdi.taskmanager.view.taskList
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import zohrevand.mahdi.customviewtest.model.CalendarTask
 import zohrevand.mahdi.customviewtest.model.Task
-import zohrevand.mahdi.taskmanager.NavigationCommand
 import zohrevand.mahdi.taskmanager.R
 import zohrevand.mahdi.taskmanager.dataAccess.TaskManagerDatabase
-import zohrevand.mahdi.taskmanager.utils.SingleLiveEvent
-import zohrevand.mahdi.taskmanager.view.newtask.NewTaskFragmentArgs
-
-import zohrevand.mahdi.taskmanager.view.taskList.dummy.DummyContent
 
 
 class TaskListFragment : Fragment() {
@@ -32,6 +26,7 @@ class TaskListFragment : Fragment() {
 
 
     private val listVewModel: TaskListVewModel by viewModel()
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +39,7 @@ class TaskListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_task_list, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         val fab: FloatingActionButton = view.findViewById(R.id.fab)
 
 
@@ -55,27 +50,32 @@ class TaskListFragment : Fragment() {
 
 
         // Set the adapter
-        if (recyclerView != null) {
-            with(recyclerView) {
-                layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-                adapter = TaskRecyclerViewAdapter(
-                    db.tasksDao,
-                    this@TaskListFragment::callBack
-                )
-                (layoutManager as LinearLayoutManager).scrollToPosition(Int.MAX_VALUE / 2)
-                PagerSnapHelper().attachToRecyclerView(this)
-            }
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            adapter = TaskRecyclerViewAdapter(
+                db.tasksDao,
+                this@TaskListFragment::onDayViewItemClickCallBack,
+                this@TaskListFragment::goToPositionCallBack
+            )
+            (layoutManager as LinearLayoutManager).scrollToPosition(Int.MAX_VALUE / 2)
+            PagerSnapHelper().attachToRecyclerView(this)
         }
+
 
 
 
         return view
     }
 
-    private fun callBack(item: CalendarTask) {
+    private fun onDayViewItemClickCallBack(item: CalendarTask) {
         Timber.i("clicked item : ${item.getTitle()}")
         val action = TaskListFragmentDirections.actionNavTasksToNavNewTask(item as Task)
         findNavController().navigate(action)
+
+    }
+
+    private fun goToPositionCallBack(position: Int) {
+        recyclerView.smoothScrollToPosition(Int.MAX_VALUE / 2)
 
     }
 }
--- app/src/main/java/zohrevand/mahdi/taskmanager/view/taskList/TaskRecyclerViewAdapter.kt
+++ app/src/main/java/zohrevand/mahdi/taskmanager/view/taskList/TaskRecyclerViewAdapter.kt
@@ -1,41 +1,44 @@
 package zohrevand.mahdi.taskmanager.view.taskList
 
-import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import timber.log.Timber
-import zohrevand.mahdi.calendar.persian.PeriodBetweenTwoTime
-import zohrevand.mahdi.customviewtest.model.CalendarTask
-
 
 //import zohrevand.mahdi.taskmanager.view.taskList.TaskListFragment.OnListFragmentInteractionListener
 
-import zohrevand.mahdi.taskmanager.business.Task
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import timber.log.Timber
+import zohrevand.mahdi.calendar.persian.PeriodBetweenTwoTime
+import zohrevand.mahdi.customviewtest.model.CalendarTask
 import zohrevand.mahdi.taskmanager.dataAccess.TasksDao
 import zohrevand.mahdi.taskmanager.dataAccess.convertDbTaskListToDayViewTaskList
 import zohrevand.mahdi.taskmanager.databinding.RowDayViewBinding
 
-/**
- * [RecyclerView.Adapter] that can display a [Task] and makes a call to the
- * specified [OnListFragmentInteractionListener].
- *
- */
+
+
+const val TodayPosition = Int.MAX_VALUE / 2
+
 class TaskRecyclerViewAdapter(
     private val tasksDao: TasksDao,
-    private val callBack: (item: CalendarTask) -> Unit
+    private val callBack: (item: CalendarTask) -> Unit,
+    private val goToPositionCallBack: ((position: Int) -> Unit)?
 ) : RecyclerView.Adapter<TaskRecyclerViewAdapter.ViewHolder>() {
 
     private val period = PeriodBetweenTwoTime()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent, callBack)
+        return ViewHolder.from(parent, callBack, goToPositionCallBack)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         var date = ""
         var age = ""
+
+
+        if (position == TodayPosition) holder.binding.goToday.visibility =
+            View.GONE else holder.binding.goToday.visibility = View.VISIBLE
         period.getDateAndAge(positionManager(position)
             , {
                 date = it
@@ -45,7 +48,7 @@ class TaskRecyclerViewAdapter(
         holder.bind(date, age, getTasksForPosition(position, holder.binding))
     }
 
-    fun getTasksForPosition(
+   private fun getTasksForPosition(
         position: Int,
         binding: RowDayViewBinding
     ): List<zohrevand.mahdi.customviewtest.model.Task> {
@@ -65,13 +68,18 @@ class TaskRecyclerViewAdapter(
 
     class ViewHolder(
         val binding: RowDayViewBinding,
-        callBack: (item: CalendarTask) -> Unit
+        callBack: (item: CalendarTask) -> Unit,
+        goToPositionCallBack: ((position: Int) -> Unit)?
     ) : RecyclerView.ViewHolder(binding.root) {
 
         init {
 
             binding.dayView.setOnItemClickListener {
                 callBack(it)
+            }
+
+            binding.goToday.setOnClickListener {
+                goToPositionCallBack?.invoke(TodayPosition)
             }
         }
 
@@ -90,10 +98,14 @@ class TaskRecyclerViewAdapter(
         }
 
         companion object {
-            fun from(parent: ViewGroup, callBack: (item: CalendarTask) -> Unit): ViewHolder {
+            fun from(
+                parent: ViewGroup,
+                callBack: (item: CalendarTask) -> Unit,
+                goToPositionCallBack: ((position: Int) -> Unit)?
+            ): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = RowDayViewBinding.inflate(layoutInflater, parent, false)
-                return ViewHolder(binding, callBack)
+                return ViewHolder(binding, callBack, goToPositionCallBack)
             }
         }
     }
